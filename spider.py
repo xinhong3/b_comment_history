56d4042bd0a28afd6fc646d6f666522b409bddbc
Xin <xinhong3@illinois.edu>
2020-12-24 00:19:50 -0600

    0.2

--- spider.py
+++ spider.py
@@ -6,50 +6,91 @@ import csv
 import sys
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
 # from selenium import JavascriptExecutor
 
 class CommentCrawler(object):
     """docstring for CommentCrawler."""
 
-    def __init__(self, website, crawlTime, runTime):
+    def __init__(self, videoSpace, crawlTime, runTime):
         super(CommentCrawler, self).__init__()
-        self.website = website
+        self.videoSpace = videoSpace
         self.crawlTime = crawlTime
         self.runTime = runTime
-        self.driver = webdriver.Chrome()
-
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
     def main(self):
-        c = open("data.csv", "w")
+
+        driver = self.driver
+
+        driver.get(self.videoSpace)
+
+        # print(spaceWindow)
+
+        lastestVideoPath = "//*[@id=\"page-index\"]/div[1]/div[1]/div/div[1]/a[2]"
+
+        # lastestVideo = driver.find_element_by_xpath(lastestVideoPath)
+
+        lastestVideo = WebDriverWait(driver,10).until(
+        			EC.presence_of_element_located((By.XPATH, lastestVideoPath))
+        		)
+
+        lastestVideoLink = lastestVideo.get_attribute('href')
+
+        last = "https://www.bilibili.com/video/BV1ny4y1D7F9"
+        
+        print(lastestVideoLink)
+
+
+
+
+        while lastestVideoLink == last:
+        	driver.refresh()
+        	lastestVideo = WebDriverWait(driver,10).until(
+        			EC.presence_of_element_located((By.XPATH, lastestVideoPath))
+        		)
+        	# lastestVideo = driver.find_element_by_xpath("//*[@id=\"page-index\"]/div[1]/div[1]/div/div[1]/a[2]")
+        	lastestVideoLink = lastestVideo.get_attribute('href')
+        	# spaceWindow = driver.current_window_handle
+        	# print(spaceWindow)
+        	print(lastestVideoLink)
+        	time.sleep(5)
+
+        spaceWindow = driver.current_window_handle
+
+        ActionChains(driver).move_to_element(lastestVideo).click().perform()
+
+        all_handles = driver.window_handles
+
+        for handle in all_handles:
+        	if handle != spaceWindow:
+        		driver.switch_to.window(handle)
+
+
+        c = open("data1.csv", "a")
 
         writer = csv.writer(c)
-
-        writer.writerow(['time', 'totalComment'])
 
         fmt = '%Y-%m-%d %H:%M'
         china = timezone('Asia/Shanghai')
 
-
-        driver = self.driver
-
-        driver.get(self.website)
-        # self.assertIn("武汉", driver.title)
-        # print(driver.title)
-
-        iter = 60*self.runTime//self.crawlTime
+        # iter = 60*self.runTime//self.crawlTime
 
 
-        for i in range(0,iter):
+        # for i in range(0,iter):
+        while True:
 
             driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
 
             try:
                 # print(1)
-                # 加载评论区
+                # load comments
                 commentElement = WebDriverWait(driver,10).until(
                     EC.presence_of_element_located((By.CLASS_NAME, "page-jump"))
                 )
@@ -57,6 +98,7 @@ class CommentCrawler(object):
                 commentPath = "//*[@id=\"comment\"]/div/div[1]/span[1]"
                 comment = driver.find_element_by_xpath(commentPath)
                 writer.writerow([currTime, comment.text])
+                print("time: {} \r comment: {} \r".format(currTime, comment.text))
                 # print(comment.text)
                 driver.refresh()
                 # print(2)
@@ -79,13 +121,15 @@ class CommentCrawler(object):
 
 if __name__ == '__main__':
 
-    # 页面
+    # old video
     website = "https://www.bilibili.com/video/BV1ny4y1D7F9"
+    # video space
+    space = "https://space.bilibili.com/390461123"
 
-    # 抓取时间间隔(s)
-    crawlTime = 60
+    # pause time(s)
+    crawlTime = 10
 
-    # 运行时间(m)
-    runTime = 60
+    # run time(m)
+    runTime = 5*60
 
-    CommentCrawler(website, crawlTime, runTime).main()
+    CommentCrawler(space, crawlTime, runTime).main()
